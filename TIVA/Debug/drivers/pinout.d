31a4e09a7cf9172f285b79ba4dfdae86c7ab5bba
Shervin <Shervin@172.19.34.65>
2016-04-20 15:48:44 +1000

    Adding Libraries by Shervin

---
+++ TIVA/Debug/drivers/pinout.d
@@ -0,0 +1,25 @@
+drivers/pinout.d: ../drivers/pinout.c \
+ C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/inc/hw_gpio.h \
+ C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/inc/hw_memmap.h \
+ C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/inc/hw_types.h \
+ C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/driverlib/gpio.h \
+ C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/driverlib/pin_map.h \
+ C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/driverlib/rom.h \
+ C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/driverlib/sysctl.h \
+ C:/Users/Shervin/git/ENB350-Assignment1/TIVA/drivers/pinout.h
+
+C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/inc/hw_gpio.h:
+
+C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/inc/hw_memmap.h:
+
+C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/inc/hw_types.h:
+
+C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/driverlib/gpio.h:
+
+C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/driverlib/pin_map.h:
+
+C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/driverlib/rom.h:
+
+C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c/driverlib/sysctl.h:
+
+C:/Users/Shervin/git/ENB350-Assignment1/TIVA/drivers/pinout.h:
--- TIVA/main.c
+++ TIVA/main.c
@@ -4,7 +4,6 @@
 
 #include <xdc/std.h>
 
-#include <xdc/runtime/Error.h>
 #include <xdc/runtime/System.h>
 
 #include <ti/sysbios/BIOS.h>
@@ -17,12 +16,10 @@
 Void taskFxn(UArg a0, UArg a1)
 {
     System_printf("enter taskFxn()\n");
-
+	
     Task_sleep(10);
-
+	
     System_printf("exit taskFxn()\n");
-
-    System_flush(); /* force SysMin output to console */
 }
 
 /*
@@ -30,18 +27,11 @@ Void taskFxn(UArg a0, UArg a1)
  */
 Int main()
 { 
-    Task_Handle task;
-    Error_Block eb;
-
+    /*
+     * use ROV->SysMin to view the characters in the circular buffer
+     */
     System_printf("enter main()\n");
-
-    Error_init(&eb);
-    task = Task_create(taskFxn, NULL, &eb);
-    if (task == NULL) {
-        System_printf("Task_create() failed!\n");
-        BIOS_exit(0);
-    }
-
+	
     BIOS_start();    /* does not return */
     return(0);
 }
